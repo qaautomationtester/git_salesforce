d9c07a5bbe4c9445b7b3986b63dab6b60845d5bb
Nishu <nishanthnayaka007@gmail.com>
2025-06-19 11:56:45 +0530

    added some more tests

---
+++ tests/forsharding.test.ts
@@ -0,0 +1,45 @@
+import { test, expect, Page } from '@playwright/test';
+import { POM } from '../pageobjectmodel/pomManager';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const BASE_URL = process.env.SWLABS_BASE_URL as string;
+const USERNAME = process.env.SWLABS_USERNAME as string;
+const PASSWORD = process.env.SWLABS_PASSWORD as string;
+
+test('Login-to-SwagLabs-1', async ({ page }: { page: Page }) => {
+  const SWPOM = new POM(page);
+  const SWLABS = SWPOM.getSwagLabs();
+
+  await SWLABS.goto(BASE_URL);
+  await SWLABS.validLogin(USERNAME, PASSWORD);
+  await SWLABS.clickOnLogin();
+
+  await page.waitForLoadState();
+
+  const title = await page.locator('.app_logo').textContent();
+  expect(title).toContain('Swag Labs');
+
+  await SWLABS.clickOnLogout();
+
+  await expect(page).toHaveURL('https://www.saucedemo.com/');
+});
+
+test('Login-to-SwagLabs-2', async ({ page }: { page: Page }) => {
+  const SWPOM = new POM(page);
+  const SWLABS = SWPOM.getSwagLabs();
+
+  await SWLABS.goto(BASE_URL);
+  await SWLABS.validLogin(USERNAME, PASSWORD);
+  await SWLABS.clickOnLogin();
+
+  await page.waitForLoadState();
+
+  const title = await page.locator('.app_logo').textContent();
+  expect(title).toContain('Swag Labs');
+
+  await SWLABS.clickOnLogout();
+
+  await expect(page).toHaveURL('https://www.saucedemo.com/');
+});
